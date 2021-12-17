1d7097ed169c0139166b8986683a80db3fa8782e
milovanderlinden <milo@dogodigi.net>
2021-12-17 14:20:38 +0100

    Clean

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@
 compile_error!("multicam (v4l2) is for linux/freebsd only");
 extern crate eye;
 use eye::prelude::*;
+use std::process::Command;
 use std::{thread, time};
-use std::process::{Command};
 
 /**
  * Main function
  */
 fn main() {
     let ctx = Context::new();
-    // Placeholder to gather threads
-    let mut threads = vec![];
+
     // Detect video devices
     let devices = ctx.query_devices().expect("Failed to query devices");
 
@@ -20,44 +19,38 @@ fn main() {
     }
 
     devices.into_iter().for_each(|_device_uri| {
-        println!("Found camera at {}", _device_uri);
         let mut _device = _device_uri.replace("v4l://", "");
+        println!("Probing for camera at {}", _device);
         let mut _name = _device.replace("/", "_");
         let mut _jpeg = format!("{}.jpeg", _name);
         let mut _mp4 = format!("{}.mp4", _name);
 
         // Each camera needs to be activated in it's own thread
-        threads.push(
-            thread::Builder::new()
-                .name(format!("multicam{}", _name))
-                .spawn(move || -> ! {
-                    let _command = Command::new("ffmpeg")
-                        .args([
-                            "-y",
-                            "-loglevel", 
-                            "quiet",
-                            "-f",
-                            "v4l2",
-                            "-video_size",
-                            "1280x720",
-                            "-i",
-                            &_device.to_owned(),
-                            &_mp4.to_owned()
-                        ])
-                        .spawn()
-                        .expect("failed to execute process");
-                    loop {
-                    } // keep running.
-                    
-                }),
-        );
+        thread::Builder::new()
+            .name(format!("multicam{}", _name))
+            .spawn(move || {
+                let _command = Command::new("ffmpeg")
+                    .args([
+                        "-y",
+                        "-loglevel",
+                        "quiet",
+                        "-f",
+                        "v4l2",
+                        "-video_size",
+                        "1280x720",
+                        "-i",
+                        &_device.to_owned(),
+                        &_mp4.to_owned(),
+                    ])
+                    .status().unwrap();
+                    println!("{} is not a valid camera: {}", _device, _command);
+            }).unwrap();
     });
 
     let ten_millis = time::Duration::from_millis(10000);
 
     loop {
         thread::sleep(ten_millis);
-        println!("Multicam is running with {:?} cameras", threads.len());
+        println!("Multicam is running with {:?} cameras", 3);
     }
-
 }
